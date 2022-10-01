b7ed709b32bfa52549f2e21ade1b41d5911f3225
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2022-10-01 19:00:41 +0300

    217. Contains Duplicate

---
+++ test/easy/contains_duplicate_test.dart
@@ -0,0 +1,21 @@
+import 'package:leetcode_dart/easy/contains_duplicate.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  group(
+    'Example tests',
+    () {
+      final cd = Solution();
+      test('true', () => expect(true, cd.containsDuplicate([1, 2, 3, 1])));
+      test('false', () => expect(false, cd.containsDuplicate([1, 2, 3, 4])));
+      test(
+        'true',
+        () => expect(
+          true,
+          cd.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]),
+        ),
+      );
+    },
+  );
+}
