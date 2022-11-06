5323337e13a9c22e81a2b5a3dd0f927c70db3ef0
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2022-11-06 08:34:52 +0300

    1325. Delete Leaves With a Given Value

---
+++ test/medium/delete_leaves_with_a_given_value_test.dart
@@ -0,0 +1,115 @@
+import 'package:leetcode_dart/medium/delete_leaves_with_a_given_value.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group(
+    'Example tests',
+    () {
+      final dlwagv = Solution();
+      test(
+        '2',
+        () {
+          final result = dlwagv.removeLeafNodes(
+            TreeNode(
+              1,
+              TreeNode(
+                2,
+                TreeNode(2),
+                null,
+              ),
+              TreeNode(
+                3,
+                TreeNode(2),
+                TreeNode(4),
+              ),
+            ),
+            2,
+          );
+          expect(
+            true,
+            _areEqual(
+              TreeNode(
+                1,
+                null,
+                TreeNode(
+                  3,
+                  null,
+                  TreeNode(4),
+                ),
+              ),
+              result,
+            ),
+          );
+        },
+      );
+      test(
+        '3',
+        () {
+          final result = dlwagv.removeLeafNodes(
+            TreeNode(
+              1,
+              TreeNode(
+                3,
+                TreeNode(3),
+                TreeNode(2),
+              ),
+              TreeNode(3),
+            ),
+            3,
+          );
+          expect(
+            true,
+            _areEqual(
+              TreeNode(
+                1,
+                TreeNode(
+                  3,
+                  null,
+                  TreeNode(2),
+                ),
+                null,
+              ),
+              result,
+            ),
+          );
+        },
+      );
+      test(
+        '2',
+        () {
+          final result = dlwagv.removeLeafNodes(
+            TreeNode(
+              1,
+              TreeNode(
+                2,
+                TreeNode(
+                  2,
+                  TreeNode(2),
+                  null,
+                ),
+              ),
+              null,
+            ),
+            2,
+          );
+          expect(
+            true,
+            _areEqual(
+              TreeNode(1),
+              result,
+            ),
+          );
+        },
+      );
+    },
+  );
+}
+
+bool _areEqual(TreeNode? f, TreeNode? s) {
+  if (f != null && s != null) {
+    return f.val == s.val &&
+        _areEqual(f.left, s.left) &&
+        _areEqual(f.right, s.right);
+  }
+  return f == null && s == null;
+}
