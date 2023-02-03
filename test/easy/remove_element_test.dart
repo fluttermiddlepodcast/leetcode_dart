9e05a4c0caea7d18532727a8c1154732b80431c6
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2023-02-03 10:37:15 +0300

    updated "27. Remove Element"

--- test/easy/remove_element_test.dart
+++ test/easy/remove_element_test.dart
@@ -4,10 +4,10 @@ import 'package:test/test.dart';
 void main() {
   group(
     'Example tests',
-        () {
+    () {
       final re = Solution();
-      test('2', () => expect(2, re.removeElement([3,2,2,3], 3)));
-      test('5', () => expect(5, re.removeElement([0,1,2,2,3,0,4,2], 2)));
+      test('2', () => expect(2, re.removeElement([3, 2, 2, 3], 3)));
+      test('5', () => expect(5, re.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)));
     },
   );
 }
